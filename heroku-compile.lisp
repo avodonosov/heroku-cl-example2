323b73b7d6ef69b2b804b640fb0f698f6f519860
Anton Vodonosov <avodonosov@yandex.ru>
2013-02-04 07:16:21 +0300

    work

--- heroku-compile.lisp
+++ heroku-compile.lisp
@@ -1,6 +1,9 @@
 (in-package :cl-user)
 
 (print ">>> Building system....")
+(require 'asdf)
+(asdf:disable-output-translations)
+(require-quicklisp)
 (ql:quickload :hunchentoot)
 (print ">>> Done building system")
 
