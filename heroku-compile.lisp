c10abde42187e619127fa88f84099e43c47c1070
Anton Vodonosov <avodonosov@yandex.ru>
2013-02-04 06:27:09 +0300

    work

--- heroku-compile.lisp
+++ heroku-compile.lisp
@@ -1,10 +1,7 @@
 (in-package :cl-user)
 
-(defun heroku-compile()
-  (print ">>> Building system....")
-  (ql:quickload :hunchentoot)
-  (print ">>> Done building system"))
-
-
+(print ">>> Building system....")
+(ql:quickload :hunchentoot)
+(print ">>> Done building system")
 
 ;; change the file just to invoke recompilation at heroku: 26
